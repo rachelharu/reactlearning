f25ee9ffff7c10b54d19bf9d2ab001cde96b74d4
rachelharu <rachel.haru9@gmail.com>
2022-12-09 12:20:30 -0800

    add form inputs

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,8 +1,9 @@
 import './NewExpense.css';
+import ExpenseForm from './ExpenseForm'
 
 const NewExpense = () => {
     return <div className="new-expense">
-        
+        <ExpenseForm />
     </div>
 }
 
