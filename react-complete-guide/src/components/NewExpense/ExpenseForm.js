005c001d2feeb19be3fe0f2a2d4b15ada01bb5d1
rachelharu <rachel.haru9@gmail.com>
2022-12-09 13:29:47 -0800

    pass data from child to parent can now be console.logged

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -37,7 +37,7 @@ const ExpenseForm = (props) => {
       date: new Date(enteredDate),
     };
 
-    props.onSaveExpenseData();
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
