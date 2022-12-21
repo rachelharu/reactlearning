913c95f4610949eda53cc045694ccbfe66aa8560
rachelharu <rachel.haru9@gmail.com>
2022-12-21 14:42:32 -0800

    create chart components

--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
-import ExpensesList from './ExpensesList'
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 function Expenses(props) {
@@ -19,6 +20,7 @@ function Expenses(props) {
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
