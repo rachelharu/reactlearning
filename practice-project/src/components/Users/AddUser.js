0fd7b0bfec751539c12cb35f6f6b6b0e5713db92
rachelharu <rachel.haru9@gmail.com>
2023-01-03 12:22:22 -0800

    create if checks for form validation

--- practice-project/src/components/Users/AddUser.js
+++ practice-project/src/components/Users/AddUser.js
@@ -10,25 +10,42 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+        return;
+    } 
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
   };
 
   const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value)
+    setEnteredUsername(event.target.value);
   };
 
   const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value)
+    setEnteredAge(event.target.value);
   };
-
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' onChange={usernameChangeHandler} />
+        <input
+          id='username'
+          type='text'
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor='age'>Age (Years)</label>
-        <input id='submit' type='number' onChange={ageChangeHandler} />
+        <input
+          id='submit'
+          type='number'
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
