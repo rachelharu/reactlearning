e6022b17e3119879b4312f78ef465d12b827ba08
rachelharu <rachel.haru9@gmail.com>
2023-01-06 14:51:12 -0800

    add useState

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,16 +1,18 @@
+import { useState } from 'react'
+
 function App() {
-  // const handleClick = () => {
-  //   console.log('Button was clicked!');
-  // };
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
 
   return (
     <div>
       <button
-        onClick={() => {
-          console.log('Button was clicked!');
-        }}>
+        onClick={handleClick}>
         Add Animal
       </button>
+      <div>Number of animals: {count}</div>
     </div>
   );
 }
