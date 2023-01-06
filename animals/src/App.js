4317bb7fd42c6a1a417d96353ac135cfbf6ce722
rachelharu <rachel.haru9@gmail.com>
2023-01-06 13:59:28 -0800

    add handleclick

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,11 +1,18 @@
 function App() {
-  const handleClick = () => {
-    console.log('Button was clicked!')
-  }
+  // const handleClick = () => {
+  //   console.log('Button was clicked!');
+  // };
 
-  return <div>
-  <button onClick={handleClick}>Add Animal</button>
-  </div>;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          console.log('Button was clicked!');
+        }}>
+        Add Animal
+      </button>
+    </div>
+  );
 }
 
 export default App;
