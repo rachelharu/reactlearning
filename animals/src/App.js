38142fb137f761b00f18bc30d96c049680775c8c
rachelharu <rachel.haru9@gmail.com>
2023-01-10 16:00:01 -0800

    add styling

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,30 +1,28 @@
+import './App.css';
 import { useState } from 'react';
 import AnimalShow from './AnimalShow';
 
 function getRandomAnimal() {
   const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
 
-  return animals[Math.floor(Math.random() * animals.length)]
+  return animals[Math.floor(Math.random() * animals.length)];
 }
 
 function App() {
- const [animals, setAnimals] = useState([]);
+  const [animals, setAnimals] = useState([]);
 
   const handleClick = () => {
-    setAnimals([...animals, getRandomAnimal()])
+    setAnimals([...animals, getRandomAnimal()]);
   };
 
   const renderedAnimals = animals.map((animal, index) => {
-    return <AnimalShow type={animal} key={index} /> 
+    return <AnimalShow type={animal} key={index} />;
   });
 
   return (
-    <div>
-      <button
-        onClick={handleClick}>
-        Add Animal
-      </button>
-      <div>{renderedAnimals}</div>
+    <div className='app'>
+      <button onClick={handleClick}>Add Animal</button>
+      <div className='animal-list'>{renderedAnimals}</div>
     </div>
   );
 }
