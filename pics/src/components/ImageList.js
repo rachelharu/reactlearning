18f3d965b35ed40d9433d320eb817d13ef8f3c93
rachelharu <rachel.haru9@gmail.com>
2023-01-18 15:50:29 -0800

    render images on page, create css files for styling

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,5 +1,16 @@
+import './ImageList.css';
+import ImageShow from './ImageShow';
+
 function ImageList({ images }) {
-  return <div>ImageList: {images.length}</div>;
+  const renderedImages = images.map((image) => {
+    return (
+    <div key={image.id}>
+      <ImageShow image={image} />
+    </div> 
+    );
+  });
+
+  return <div  className="image-list">{renderedImages}</div>;
 }
 
 export default ImageList;
