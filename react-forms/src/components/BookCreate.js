938e94f9724ea288df7fd97eca2d00d60923ccf2
rachelharu <rachel.haru9@gmail.com>
2023-01-30 15:13:32 -0800

    add event handlers to BookCreate

--- react-forms/src/components/BookCreate.js
+++ react-forms/src/components/BookCreate.js
@@ -1,8 +1,22 @@
-function BookCreate() {
+import { useState } from 'react';
+
+function BookCreate({ onCreate }) {
+  const[title, setTitle] = useState('');
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+  };
+
   return <div>
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>Title</label>
-      <input />
+      <input value={title} onChange={handleChange} />
+      <button>Create!</button> 
     </form>
   </div>;
 }
