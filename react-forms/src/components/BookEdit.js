c7b43a90a3b445143f1009e596e773080f0af712
rachelharu <rachel.haru9@gmail.com>
2023-02-06 15:43:03 -0800

    pass onEdit prop down to BookEdit

--- react-forms/src/components/BookEdit.js
+++ react-forms/src/components/BookEdit.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
-function BookEdit() {
-  const [title, setTitle] = useState('');
+function BookEdit({ book, onEdit }) {
+  const [title, setTitle] = useState(book.title);
 
   const handleChange = (event) => {
     setTitle(event.target.value);
@@ -10,8 +10,8 @@ function BookEdit() {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    console.log('New title', title);
-  };
+    onEdit(book.id, title);
+  }
 
   return <form onSubmit={handleSubmit} className="book-edit">
   <label>Title</label>
