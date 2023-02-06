ba179fabff845eb9dcd612ffa10e81312325f4a3
rachelharu <rachel.haru9@gmail.com>
2023-02-06 15:22:50 -0800

    change handleClick to handleDeleteClick. create handleEditClick. add useState.

--- react-forms/src/components/BookShow.js
+++ react-forms/src/components/BookShow.js
@@ -1,17 +1,35 @@
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
 function BookShow({ book, onDelete }) {
-  const handleClick = () => {
+  const [showEdit, setShowEdit] = useState(false);
+
+  const handleDeleteClick = () => {
     onDelete(book.id);
   };
 
-  return <div className="book-show">
-  {book.title}
-  <div className="actions">
-    <button className="delete" onClick={handleClick}> 
-    Delete
-    </button> 
-  </div>
-  
-  </div>;
+  const handleEditClick = () => {
+    setShowEdit(!showEdit);
+  }
+
+  let content = <h3>{book.title}</h3>;
+  if (showEdit) {
+    content = <BookEdit />;
+  }
+
+  return (
+    <div className='book-show'>
+      <div>{content}</div>
+      <div className='actions'>
+      <button className="edit" onClick={handleEditClick}>
+        Edit
+      </button>
+        <button className='delete' onClick={handleDeleteClick}>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default BookShow;
