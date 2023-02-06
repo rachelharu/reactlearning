c0344004995d04807394dfea6b0b15fbecc857e6
rachelharu <rachel.haru9@gmail.com>
2023-02-06 15:28:05 -0800

    add event handlers

--- react-forms/src/components/BookEdit.js
+++ react-forms/src/components/BookEdit.js
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+
 function BookEdit() {
-  return <div>BookEdit</div>;
+  const [title, setTitle] = useState('');
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('New title', title);
+  };
+
+  return <form onSubmit={handleSubmit} className="book-edit">
+  <label>Title</label>
+  <input className="input" value={title} onChange={handleChange} />
+  <button className="button is-primary">
+    Save
+  </button>
+  </form>;
 }
 
 export default BookEdit;
