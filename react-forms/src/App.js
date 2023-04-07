636b565a275661166da11988a0de547a7856d76e
rachelharu <rachel.haru9@gmail.com>
2023-04-07 15:36:25 -0700

    refactor app.js and books.js

--- react-forms/src/App.js
+++ react-forms/src/App.js
@@ -4,66 +4,17 @@ import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
-  const [books, setBooks] = useState([]);
-
-  const fetchBooks = async () => {
-      const response = await axios.get('http://localhost:3002/books'
-      );
-
-      setBooks(response.data);
-  };
-
   useEffect(() => {
     fetchBooks();
   }, []);
-  
-
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3002/books/${id}`, {
-      title: newTitle
-    });
-
-    console.log(response);
-
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, ...response.data };
-      }
-
-      return book;
-    });
-
-    setBooks(updatedBooks);
-  };
-   
-  const deleteBookById = async (id) => {
-   await axios.delete(`http://localhost:3002/books/${id}`);
-
-    const updatedBooks = books.filter((book) => { 
-      return book.id !== id;
-    });
-
-    setBooks(updatedBooks);
-  };
-
-  const createBook = async (title) => {
-    const response = await axios.post('http://localhost:3002/books', {
-      title,
-    });
-
-    const updatedBooks = [
-      ...books, response.data
-    ];
-    setBooks(updatedBooks);
-  };
 
   return (
    <div className="app">
    <h1>
    Reading List
    </h1>
-    <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
-    <BookCreate  onCreate={createBook} />
+    <BookList />
+    <BookCreate />
   </div>
 );}
 
--- react-forms/src/index.js
+++ react-forms/src/index.js
@@ -8,7 +8,7 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 root.render(
-  <Provider value={{ }}>
+  <Provider>
     <App />
   </Provider>
 );
