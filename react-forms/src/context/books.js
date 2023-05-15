aaa63e00aa025f849c5eb7bcdd7affcd83621353
rachelharu <rachel.haru9@gmail.com>
2023-05-15 11:23:16 -0700

    update useCallback for fetchImages

--- react-forms/src/context/books.js
+++ react-forms/src/context/books.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useCallback } from 'react';
 import axios from 'axios';
 
 const BooksContext = createContext();
@@ -6,12 +6,13 @@ const BooksContext = createContext();
 function Provider({ children }) {
     const [books, setBooks] = useState([]);
 
-  const fetchBooks = async () => {
+  const fetchBooks = useCallback(async () => {
       const response = await axios.get('http://localhost:3002/books'
       );
 
       setBooks(response.data);
-  };
+  }, []);
+
 
   const editBookById = async (id, newTitle) => {
     const response = await axios.put(`http://localhost:3002/books/${id}`, {
